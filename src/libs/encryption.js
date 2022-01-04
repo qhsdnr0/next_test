b2787cf4c02bbd9cbdac571f7aa8b06b62ca5499
qhsdnr0 <qhsdnr0@gmail.com>
2022-01-05 07:57:17 +0900

    Add : 로그인 기능 추가
    - 로그인 기능 추가

--- src/libs/encryption.js
+++ src/libs/encryption.js
@@ -1,15 +1,24 @@
 const crypto = require('crypto');
-
+const bcrypt = require('bcrypt');
+const SALTROUND = 10;
 
 module.exports = {
     //salt 생성
-    makeSalt: () => {
-        return crypto.randomBytes(32).toString('hex');
-    },
+    // makeSalt: () => {
+    //     return crypto.randomBytes(32).toString('hex');
+    // },
 
     //암호화
-    encrypt: (password, salt) => {
-        const key = crypto.pbkdf2Sync(password, salt.toString(), 100000, 32, 'sha512');
-        return key.toString('hex');
+    encrypt: async (password) => {
+    //     const key = crypto.pbkdf2Sync(password, salt.toString(), 100000, 32, 'sha512');
+    //     return key.toString('hex');
+        // const salt = bcrypt.genSalt(SALTROUND);
+        return bcrypt.hashSync(password, SALTROUND);
+        
     },
+
+    //비밀번호 확인
+    checkPw: async (password, hashedPassword) => {
+        return bcrypt.compareSync(password, hashedPassword);
+    }
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,81 +1,54 @@
 const { Op } = require("sequelize");
 const User = require('../models').user;
+const encryption = require('../libs/encryption.js');
 
-// /**
-//  * 회원가입 서비스
-//  * @param {String} email
-//  * @param {String} password
-//  * @param {String} nickName
-//  * @returns {Object} 가입한 유저 정보 { email, password, nickName }
-//  */
 exports.signup = async (
   email,
   password,
   nickName,
 ) => {
   try {
-    // console.log(1);
     const newUser = await User.create({
       email: email,
       password: password,
       nickName: nickName,
     });
-    console.log(1);
     return newUser;
   } catch (err) {
     throw err;
   }
 };
 
-// /**
-//  * 이메일 체크 서비스
-//  * @param {String} email
-//  * @returns {Object} 
-//  */
 exports.checkEmail = async (email) => {
   try {
-    console.log(1);
     const alreadyUser = await User.findOne({
       where: {
         email
       }
     });
-    console.log(1);
     return alreadyUser;
   } catch (err) {
     throw err;
   }
 };
 
-/**
- * 로그인 서비스
- * @param {String} emailUsername
- * @param {String} emailDomain
- * @param {String} password
- * @returns {Object} 로그인한 유저 정보 { username, domain, password, isAdmin, salt, refreshToken, createdAt, updatedAt }
- */
-exports.signin = async (emailUsername, emailDomain, password) => {
-  try {
-    const user = await models.user.findOne({
-      where: {
-        [Op.and]: [
-          { username: emailUsername },
-          { domain: emailDomain },
-          { password: password }
-        ],
-      },
-    });
-    return user.dataValues;
-  } catch (err) {
+exports.signin = async (email, password) => {
+    try {
+        const user = await User.findOne({
+            where: {
+            email
+            },
+        });
+
+    
+        const hashedPassword = user.password;
+        if (await encryption.checkPw(password, hashedPassword)) {
+            return user.dataValues;
+        } else {
+            return false;
+        }        
+    
+    } catch (err) {
     throw err;
-  }
+    }
 };
-
-// exports.updateRefreshToken = async (emailUsername, emailDomain, refreshToken) => {
-//   try {
-
-//     return user;
-//   } catch (err) {
-//     throw err;
-//   }
-// }
